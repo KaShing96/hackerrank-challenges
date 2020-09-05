a178e1e2f35133d6797090ad98da5a9ff431e632
RotiCanaiGuy <31625792+RotiCanaiGuy@users.noreply.github.com>
2020-09-05 15:03:20 +0800

    divisible sum pairs

---
+++ easy/divisible_sum_pairs/README.md
@@ -0,0 +1,7 @@
+# Divisible Sum Pairs
+*This README.md is instantiated by HackerRank Coding Template Version 4.0
+
+This problem can be found [here](https://www.hackerrank.com/challenges/divisible-sum-pairs/problem?utm_campaign=challenge-recommendation).
+
+# Solution
+The solution asks for `(i, j)` pairs where `i < j`. Thus, we sort the array `ar` first. Then, we use two nested `for` loops to iterate through each possible element.
---
+++ easy/divisible_sum_pairs/functions.py
@@ -0,0 +1,140 @@
+# === Imports ===
+import math
+import os
+import random
+import re
+import sys
+
+# === Function ===
+def divisibleSumPairs(n, k, ar):
+    # Sort
+    ar = sorted(ar)
+
+    # Answers 
+    ans = 0
+
+    # Nested loop
+    for ix in range(n): 
+        
+        # Set i
+        i = ar[ix]
+
+        for jx in range(ix + 1, n): 
+
+            # Set j
+            j = ar[jx]
+
+            # Ensure that ix and jx are not equal, and that ix < jx
+            if ix >= jx:
+                continue 
+
+            # Check for divisibility
+            if (i + j) % k == 0: 
+
+                ans += 1
+
+    return ans             
+
+
+# Function to be called
+def main(args):
+    """
+    This is the main function to be called in test_functions.py.
+    This should emulate the logic in HackerRank's if __name__ == '__main__' logic block and process #args# accordingly. 
+
+    Params
+    ======
+    args: str
+        A single line string
+    """
+
+    nk = input().split()
+
+    n = int(nk[0])
+
+    k = int(nk[1])
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = divisibleSumPairs(n, k, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+ 
+
+
+# === Debug ===
+def DEBUG(*args, **kwargs):
+    """
+    If this function is not run directly, i.e. it is under test, this will take on the print statement. Otherwise, nothing happens. 
+    """
+    if __name__ != "__main__":
+        print(*args, **kwargs)
+
+
+# === Mock ===
+# Mock fptr.write()
+class Writer():
+    def __init__(self):
+        """Initialises the list of answers."""
+        self.answers = []
+
+
+    def write(self, string):
+        """Appends the string to a list, which is then accessed by the parent test function to check for equality of arguments.
+        
+        Params
+        ======
+        string: str
+            The string to be appended to the list of answers.
+        """
+        # Process the string to be appended, to remove the final newline character as added in main()
+        li = string.rsplit('\n', 1)
+
+        self.answers.append(''.join(li))
+
+    
+    def get_answers(self): 
+        """
+        Returns the answers and resets it.
+
+        Returns
+        =======
+        result: list of strings
+            The answers to be returned.
+        """
+        result = self.answers 
+
+        self.answers = []
+
+        return result
+
+
+    def close(self):
+        pass
+        
+
+fptr = Writer()
+
+# List of inputs
+list_of_inputs = []
+
+# Sets inputs
+def set_inputs(string):
+    """
+    This function sets the inputs to be mocked by the input() function. 
+    The #string# passed is split by the newline character. Each element then makes up the argument called sequentially by input().
+    """
+    global list_of_inputs
+
+    list_of_inputs = string.split("\n")
+
+
+# Mocks the inputs
+def input():
+    """
+    Mocks the 'input()' function. 
+    If arguments is not None, it resets the arguments used. 
+    """
+    return list_of_inputs.pop(0)
